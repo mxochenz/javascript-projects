c4e98a884fcee6f496b8d1ca581fbf2ed5f520c1
mxochenz <jose.garranas@hotmail.com>
2025-05-08 12:14:10 +0200

    feat: added function to twitter and new quote buttons

--- Project 1 - Quote Generator/Template - quote-generator/script.js
+++ Project 1 - Quote Generator/Template - quote-generator/script.js
@@ -8,8 +8,17 @@ let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
+  // Pick a random quote from apiQuotes array
   const randomQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(randomQuote);
+
+  authorText.textContent = randomQuote.author || "Unknown";
+  // Check quote length to determine styling
+  if (randomQuote.quote.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = randomQuote.quote;
 }
 
 // Get Quotes from API
@@ -19,13 +28,23 @@ async function getQuotes() {
   try {
     const response = await fetch(apiURL);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
 
     newQuote();
   } catch (error) {
     // catch the error
+    alert("Failed to load quotes. Please try again later.");
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterURL, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
